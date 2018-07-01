4201bb080e31c08e24a0ec206d3e6cf2cadfed31
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-01 19:13:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -63,7 +63,8 @@ def shortened_tweet_truncator(tweet)
   elsif word_substituter(tweet).length > 140
     return word_substituter(tweet[0..140])
   else return word_substituter(tweet)
-        binding.pry 
+       
 
 end 
+ binding.pry 
 end
